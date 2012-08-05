d0b070c8d3123cf66b976a194d1f03687ec732d1
David Boskovic <david@momentumapp.co>
2012-08-05 13:29:48 -0700

    initial commit

---
+++ javascripts/app.js
@@ -0,0 +1,42 @@
+(function($){  
+  $(function(){
+    
+    
+    
+    $(document).foundationButtons();
+    
+    
+    
+    
+    
+    $(document).foundationCustomForms();
+    
+    
+    
+    
+    
+    
+    // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+    // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
+    // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
+    // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
+    // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
+  });
+  
+})(jQuery);
+
+
+/* Exo Webfont */
+
+WebFontConfig = {
+google: { families: [ 'Exo:200:latin' ] }
+};
+(function() {
+var wf = document.createElement('script');
+wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+wf.type = 'text/javascript';
+wf.async = 'true';
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(wf, s);
+})();
